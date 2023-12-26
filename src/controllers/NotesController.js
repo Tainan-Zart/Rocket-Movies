821bd8b62d1260babd1f117df8d24e50d35b0c14
Tainan <tainanzart713@gmail.com>
2023-12-26 11:26:25 -0300

    Rating check

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,37 +1,42 @@
 const knex = require("../database/knex")
 
 class NotesController{
-  async create(request, response){
-    const {title, description, rating, tags} = request.body
-    const {user_id} = request.params;
-
-     // Verifica se o rating é um número inteiro entre 1 e 5
-     if (!Number.isInteger(rating) || rating < 1 || rating > 5) {
-      return response.status(400).json({ error: 'Rating must be an integer between 1 and 5.' });
-    }
-
-
-    const [note_id] = await knex("movie_notes").insert({
-      title,
-      description,
-      rating,
-      user_id
-    })
-
-    const tagsInsert = tags.map(name => {
-      return{
-        note_id,
-        name,
-        user_id
-        
+  
+    async create(request, response) {
+      const { title, description, tags } = request.body;
+      const { user_id } = request.params;
+  
+      // Obtemos a propriedade 'rating' do corpo da requisição
+      const rating = request.body.rating;
+  
+      // Converte a string de rating para um número inteiro
+      const parsedRating = parseInt(rating, 10);
+  
+      // Verifica se parsedRating é um número inteiro entre 1 e 5
+      if (!Number.isInteger(parsedRating) || parsedRating < 1 || parsedRating > 5) {
+        return response.status(400).json({ error: 'Rating must be an integer between 1 and 5.' });
       }
-    });
-
-    await knex("movie_tags").insert(tagsInsert);
-
-    response.json();
-
-  }
+  
+      const [note_id] = await knex("movie_notes").insert({
+        title,
+        description,
+        rating: parsedRating,
+        user_id
+      });
+  
+      const tagsInsert = tags.map(name => {
+        return {
+          note_id,
+          name,
+          user_id
+        };
+      });
+  
+      await knex("movie_tags").insert(tagsInsert);
+  
+      response.json();
+    }
+  
 
   async show(request, response){
    const {id} = request.params
